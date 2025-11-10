6aff26801d6f8736ebe6afcb7e78c91a8ee5a62f
ElenaYurova21 <59369268+hellyu21@users.noreply.github.com>
2025-11-11 01:37:41 +0200

    fixed

---
+++ lab1/lab1/lab1/lab1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <time.h>
+#include <thread>
+#include <mutex>
+#include <string>
+using namespace std;
+
+void Func(string name)
+{
+    for (int i = 0; i <= 10000000; i++) {
+        int factorial = 1;
+        for (int j = 2; j <= 10; j++) {
+            factorial *= j;
+        }
+    }
+}
+
+
+int main()
+{
+    clock_t start = clock();
+
+    std::thread thread1(Func, "t1");
+    /*std::thread thread2(Func, "t2");*/
+    thread1.join();
+    /*thread2.join();*/
+
+    clock_t end = clock();
+    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+    cout << seconds<< endl;
+
+    system("pause");
+}
---
+++ lab2/lab2/lab2/lab2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <string>
+using namespace std;
+
+//std::mutex m;
+
+void Func(string name) {
+    long double i = 0;
+
+    auto start = chrono::steady_clock::now();
+    auto end = start + chrono::seconds(1);
+
+    while (chrono::steady_clock::now() < end) {
+        i += 1e-9;
+    }
+
+    //m.lock();
+    cout << name << ": " << i << endl;
+    //m.unlock();
+}
+
+int main()
+{
+    std::thread thread1(Func, "t1");
+    std::thread thread2(Func, "t2");
+    std::thread thread3(Func, "t3");
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    system("pause");
+}
